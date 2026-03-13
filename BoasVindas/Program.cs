1d0e66ba5136c09d573661eab84ee8adc5e24819
Irenita-Lopes <irenita.f.lopes@gmail.com>
2026-03-13 15:06:49 -0300

    Add project files.

---
+++ BoasVindas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Saudacao
+{
+    private readonly string nomeCliente;
+
+    public Saudacao(string nomeCliente)
+    {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public string ObterMensagem()
+    {
+        return $"Ola {nomeCliente}, seja bem-vindo(a)!";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+      
+        Console.Write("Digite seu nome aqui: ");
+
+        string nome = Console.ReadLine();
+
+        Saudacao s = new Saudacao(nome);
+
+        Console.WriteLine(s.ObterMensagem());
+    }
+}
